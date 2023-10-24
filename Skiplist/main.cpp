e2f21ef3ec54b8e3230ea526ff28460f2556ebc3
Zhang-nin <840277847@qq.com>
2023-10-24 20:07:10 +0800

    projects

---
+++ Skiplist/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "skiplist.h"
+#define FILE_PATH "./store/dumpFile"
+
+int main() {
+
+    // 键值中的key用int型，如果用其他类型，需要自定义比较函数
+    // 而且如果修改key的类型，同时需要修改skipList.load_file函数
+    SkipList<int, std::string> skipList(6);
+	skipList.insert_element(1, "c++"); 
+	skipList.insert_element(3, "python"); 
+	skipList.insert_element(7, "go"); 
+	skipList.insert_element(8, "sql"); 
+	skipList.insert_element(9, "linux"); 
+	skipList.insert_element(19, "tcp"); 
+	skipList.insert_element(19, "udp"); 
+
+    std::cout << "skipList size:" << skipList.size() << std::endl;
+
+    skipList.dump_file();
+
+    // skipList.load_file();
+
+    skipList.search_element(9);
+    skipList.search_element(18);
+
+
+    skipList.display_list();
+
+    skipList.delete_element(3);
+    skipList.delete_element(7);
+
+    std::cout << "skipList size:" << skipList.size() << std::endl;
+
+    skipList.display_list();
+}
